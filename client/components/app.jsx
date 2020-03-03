6010c34fe852cb1b211b437f150f20c4c52135a1
blake-ros <dev@MSI.localdomain>
2020-03-02 16:39:20 -0800

    completed header

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // const grades = [];
+    this.state = ({
+
+    });
+
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
-    return null;
+    return (
+      <h1>Student Grade Table</h1>
+      // <table>
+      //   <thead>
+      //     <td>Student Name</td>
+      //     <td>Course</td>
+      //     <td>Grade</td>
+      //   </thead>
+      //   <tbody></tbody>
+      // </table>
+    );
   }
 }
 
