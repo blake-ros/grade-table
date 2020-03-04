f759d45ec4c78c0593cdc1cddcb34b3e5bf6795a
blake-ros <dev@MSI.localdomain>
2020-03-03 17:58:09 -0800

    added state to grade-form component

--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -5,32 +5,42 @@ class GradeForm extends React.Component {
     super(props);
     this.state = ({
       name: '',
-      grade: '',
+      grade: null,
       course: ''
     });
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
   }
 
-  render() {
+  handleChange(event) {
+    this.setState({
+      name: event.target.value,
+      grade: event.target.value,
+      course: event.target.value
+    });
+  }
 
+  render() {
+    console.log(this.state);
     return (
       <form className="col-md-3" onSubmit={this.props.addGrade()}>
         <h2 className="ml-2 mt-1 pb-2" id="addGrade">Add Grade</h2>
         <div className="form-group ml-2">
           <div className="input-group mb-3">
             <i className="fas fa-user fa-lg input-group-prepend input-group-text d-flex"></i>
-            <input name="name" className="form-control form-control-lg" type="text" placeholder="Name" id="name"></input>
+            <input name="name" className="form-control form-control-lg" type="text" placeholder="Name" id="name" value={this.state.value} onChange={this.handleChange}></input>
           </div>
           <div className="input-group mb-3">
             <i className="far fa-list-alt fa-lg input-group-prepend input-group-text d-flex"></i>
-            <input name="course" className="form-control form-control-lg" type="text" placeholder="Course" id="course"></input>
+            <input name="course" className="form-control form-control-lg" type="text" placeholder="Course" id="course" value={this.state.value} onChange={this.handleChange}></input>
           </div>
           <div className="input-group mb-3">
             <i className="fas fa-graduation-cap fa-md input-group-prepend input-group-text d-flex"></i>
-            <input name="grade" className="form-control form-control-lg" type="text" placeholder="Grade" id="grade"></input>
+            <input name="grade" className="form-control form-control-lg" type="text" placeholder="Grade" id="grade" value={this.state.value} onChange={this.handleChange}></input>
           </div>
           <button type="submit" className="btn btn-success btn-sm mr-2 pr-3 pl-3 pt-2 pb-2" id="add">Add</button>
           <button type="reset" className="btn btn-outline-secondary btn-sm pl-2 pr-2 pt-2 pb-2">Cancel</button>
